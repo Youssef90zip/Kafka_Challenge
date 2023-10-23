c8a9acd4ab69911bc35e6f89f305134c8e591ae1
youssef <youssefaferdou90@gmail.com>
2023-10-23 10:53:34 +0200

    Anpassung

--- consumer.py
+++ consumer.py
@@ -3,18 +3,17 @@ from collections import defaultdict
 import json
 
 
-
-
 # Herstellung der Verbindung zum Kafka-Cluster und abonnieren das Topic wikipedia-changes
 consumer = KafkaConsumer("wikipedia-changes", bootstrap_servers="localhost:9092", group_id="wiki-edits-group")
 
 global_edits_per_minute = defaultdict(int)
 german_edits_per_minute = defaultdict(int)
 
-previous_minute = None
+previous_minute = None # initialisierung von der vorherigen Minute (um den Unterschied zwischen der aktuellen Minute und vorherigen Minute zu machen)
 
 for message in consumer:
     data = json.loads(message.value.decode("utf-8"))
+    # wir greifen auf den Value von timestamp
     timestamp = data["timestamp"]
 
     # wir extrahieren  die Sprache aus der Domäne, um die deutsche Wikipedia identifizieren zu können
@@ -23,7 +22,7 @@ for message in consumer:
     else:
         german_wikipedia = False
 
-    # Ermitteln Sie die Minute des Timestamps
+    # wir ermitteln die Minute des Timestamps
     minute = timestamp // 60
 
     # Wenn es eine neue Minute ist, wird die Zählungen aktualisiert
@@ -31,14 +30,13 @@ for message in consumer:
         global_edits_per_minute[minute] = 1
         if german_wikipedia:
             german_edits_per_minute[minute] = 1
-        previous_minute = minute
+        previous_minute = minute   # hier wird die vorherige Minute auf die aktuelle Minute aktualisiert
     else:
         global_edits_per_minute[minute] += 1
         if german_wikipedia:
             german_edits_per_minute[minute] += 1
 
-    # Speichern Sie die Ergebnisse in Ihrer Datenbank oder Datei
-    # Hier drucken wir sie nur auf der Konsole aus
+    # Ausgabe von golaben und deutschen Edits
     print(f"Global Edits Per Minute: {global_edits_per_minute}")
     print(f"German Wikipedia Edits Per Minute: {german_edits_per_minute}")
 
